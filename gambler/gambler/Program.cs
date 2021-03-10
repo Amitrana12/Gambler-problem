0169c72009720a5ef4aa1b43a156906ed629832d
amit <[amirana14325@gmail.com]>
2021-03-10 11:51:49 +0530

    [Amit rana]Added:: adding variable

---
+++ gambler/gambler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost = 100;
+            int betCost = 1;
+            Console.WriteLine(cost + "    " + betCost);
+        }
+    }
+}
