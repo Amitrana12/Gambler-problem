88127a4a7888f8818108e5b76fcd5a941536114a
amit <[amirana14325@gmail.com]>
2021-03-10 13:01:54 +0530

    [AMIT RANA]ADDED:: set a limit for win money for a day

--- gambler/gambler/Program.cs
+++ gambler/gambler/Program.cs
@@ -4,25 +4,38 @@ namespace gambler
 {
     class Program
     {
-        
+        public static int totalCost = 100;
+       public static  int betCost = 1;
+      
+
         static void Main(string[] args)
         {
-            int totalCost = 100;
-            int betCost = 1;
-            Random random = new Random();
-            int Check = random.Next(0, 2);
-            if (Check == 1 && betCost==1)
-            {
-                Console.WriteLine("you win");
-                totalCost++;
-                Console.WriteLine("you left cost "+totalCost);
-            }
-            else {
-                Console.WriteLine("you lose");
-                totalCost--;
-                Console.WriteLine("you left cost " + totalCost);
-            }
+            
+            
+            winOrLose();
 
+
+        }
+        public static void winOrLose()
+        {
+           
+            while (totalCost != 150 && totalCost != 50)
+            {
+                Random random = new Random();
+                int Check = random.Next(0, 2);
+                if (Check == 1 && betCost == 1 )
+                {
+                    Console.WriteLine("you win");
+                    totalCost++;
+                    Console.WriteLine("you left cost " + totalCost);
+                }
+                else
+                {
+                    Console.WriteLine("you lose");
+                    totalCost--;
+                    Console.WriteLine("you left cost " + totalCost);
+                }
+            }
         }
     }
 }
