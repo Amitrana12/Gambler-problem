ee0fd184243f8140da14c5e69c27516f98c7744a
amit <[amirana14325@gmail.com]>
2021-03-10 12:16:51 +0530

    [Amit RAna]Added:: win and lose

--- gambler/gambler/Program.cs
+++ gambler/gambler/Program.cs
@@ -4,11 +4,25 @@ namespace gambler
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             int cost = 100;
             int betCost = 1;
-            Console.WriteLine(cost + "    " + betCost);
+            Random random = new Random();
+            int Check = random.Next(0, 2);
+            if (Check == 1)
+            {
+                Console.WriteLine("you win");
+                cost++;
+                Console.WriteLine("you left cost "+cost);
+            }
+            else {
+                Console.WriteLine("you lose");
+                cost--;
+                Console.WriteLine("you left cost " + cost);
+            }
+
         }
     }
 }
