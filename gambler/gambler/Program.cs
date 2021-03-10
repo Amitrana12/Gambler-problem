80a22d24f926f2184a01370dc13635be8b732af9
amit <[amirana14325@gmail.com]>
2021-03-10 15:21:48 +0530

    [AMIT RANA]Added::added 20day tottal amout and tootal win and losee

--- gambler/gambler/Program.cs
+++ gambler/gambler/Program.cs
@@ -2,40 +2,71 @@
 
 namespace gambler
 {
-    class Program
+   public class Program
     {
-        public static int totalCost = 100;
-       public static  int betCost = 1;
-      
+        public static  int TOTAL_COST = 100;
+        public static  int BET_COST = 1;
+        public static int day = 20;
+        public static int tottalAmout20daywin = 0;
+        public static int tottalAmout20daylose = 0;
+        public static int tottalbet20dayWin = 0;
+        public static int tottalbet20daylose = 0;
+
+
 
         static void Main(string[] args)
         {
-            
-            
-            winOrLose();
-
-
+            for (int i = 1; i <= day; i++)
+            { 
+                winOrLose();
+                day--;
+                tottalAmout20daywin += tottalAmout20daywin;
+                tottalAmout20daylose += tottalAmout20daylose;
+                tottalbet20dayWin += tottalbet20dayWin;
+                tottalbet20daylose += tottalbet20daylose;
+                    
+            }
+            Console.WriteLine("total amout for 20 day win " + tottalAmout20daywin);
+            Console.WriteLine("total amout for 20 day win " + tottalAmout20daylose);
+            Console.WriteLine("totall 20 day win bet=" + tottalbet20dayWin);
+            Console.WriteLine("total 20 losee bet=" + tottalbet20daylose);
         }
+
+
+
+
         public static void winOrLose()
         {
-           
-            while (totalCost != 150 && totalCost != 50)
+            int dayCost = TOTAL_COST;
+            int betCostOnce = BET_COST;
+            int totalBetADay = 0;
+            
+            while (dayCost != 150 && dayCost != 50)
             {
                 Random random = new Random();
                 int Check = random.Next(0, 2);
-                if (Check == 1 && betCost == 1 )
+                totalBetADay++;
+                if (Check == 1 && betCostOnce == 1 )
                 {
-                    Console.WriteLine("you win");
-                    totalCost++;
-                    Console.WriteLine("you left cost " + totalCost);
+                   // Console.WriteLine("you win");
+                    dayCost++;
+                    tottalbet20dayWin++;
+                    tottalAmout20daywin++;
+                   // Console.WriteLine("you left cost " + dayCost);
                 }
                 else
                 {
-                    Console.WriteLine("you lose");
-                    totalCost--;
-                    Console.WriteLine("you left cost " + totalCost);
+                    //Console.WriteLine("you lose");
+                    dayCost--;
+                    tottalbet20daylose++;
+                    tottalAmout20daylose++;
+        
+                    //Console.WriteLine("you left cost " + dayCost);
                 }
+               
             }
+
+           
         }
     }
 }
