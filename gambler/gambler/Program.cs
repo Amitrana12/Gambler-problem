212a3f1ea5dc128f591477fe6114b10138b036f6
amit <[amirana14325@gmail.com]>
2021-03-10 17:26:54 +0530

    [AMIT RANA]Added::udate laste uc

--- gambler/gambler/Program.cs
+++ gambler/gambler/Program.cs
@@ -6,6 +6,7 @@ namespace gambler
     {
         public static  int TOTAL_COST = 100;
         public static  int BET_COST = 1;
+
         public static int day = 20;
         public static int tottalAmout20daywin = 0;
         public static int tottalAmout20daylose = 0;
@@ -27,7 +28,7 @@ namespace gambler
                     
             }
             Console.WriteLine("total amout for 20 day win " + tottalAmout20daywin);
-            Console.WriteLine("total amout for 20 day win " + tottalAmout20daylose);
+            Console.WriteLine("total amout for 20 day lose " + tottalAmout20daylose);
             Console.WriteLine("totall 20 day win bet=" + tottalbet20dayWin);
             Console.WriteLine("total 20 losee bet=" + tottalbet20daylose);
         }
@@ -39,13 +40,13 @@ namespace gambler
         {
             int dayCost = TOTAL_COST;
             int betCostOnce = BET_COST;
-            int totalBetADay = 0;
+            
             
             while (dayCost != 150 && dayCost != 50)
             {
                 Random random = new Random();
                 int Check = random.Next(0, 2);
-                totalBetADay++;
+               
                 if (Check == 1 && betCostOnce == 1 )
                 {
                    // Console.WriteLine("you win");
